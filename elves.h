cddf364a80e430a1417b463f1583f56dbaef391b
j649wang <j649wang@users.noreply.github.com>
2016-07-15 21:06:39 -0400

    Update elves.h

--- elves.h
+++ elves.h
@@ -4,7 +4,7 @@
 
 class Elves: public Player{
   public:
-	Elves(int row=0, int col=0,int chamber=0);
+	Elves(std::shared_ptr<Cell> cell);
 	void getWealth()const override;
 	const int maxHP()const override;
 	const int maxATK()const override;
